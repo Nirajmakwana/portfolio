819ac32cd06cf10070550c3057582cb86c97f2fb
Nirajmakwana <makwananiraj12994@gmail.com>
2026-09-03 13:04:22 +0530

    fix resume download

--- src/constants/index.js
+++ src/constants/index.js
@@ -34,7 +34,7 @@ export const MyLinkedin = "https://www.linkedin.com/in/nirajmakwana12/";
 export const MyGithub = "https://github.com/Nirajmakwana";
 export const MyLocation = "India";
 export const YearsExperience = "5+";
-export const dynamicResumeName = `${MyName.replace(/\s+/g, "-")}-Resume(${new Date().getFullYear()}).pdf`;
+export const dynamicResumeName = `${MyName.replace(/\s+/g, "-")}-Resume(2026).pdf`;
 
 /**
  * Categorized technical skills prioritizing Core Frontend
